0ab69eaaf66e03bfd98bbcf7f125ca8677ed8774
rikutofis <rikuto_kimura@fis.edu>
2022-07-31 21:11:00 +0200

    Login and Sign Up

--- app/src/main/java/com/example/ia_computerscience/Controller/LoginActivity.java
+++ app/src/main/java/com/example/ia_computerscience/Controller/LoginActivity.java
@@ -2,7 +2,12 @@ package com.example.ia_computerscience.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ia_computerscience.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,9 +18,61 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    private EditText txtEmail;
+    private EditText txtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        txtEmail = findViewById(R.id.Login_txtEmail);
+        txtPassword = findViewById(R.id.Login_txtPassword);
     }
+    
+    public void login(View view) {
+        if(!formValid()) {
+            return;
+        }
+
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(this, MyRecipeActivity.class));
+                    }
+                    else {
+                        Toast.makeText(this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        Log.w(TAG, task.getException().getMessage(), task.getException());
+                    }
+                });
+    }
+
+    private boolean formValid() {
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        if(email.equals("")) {
+            Toast.makeText(this, "Email is empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(password.equals("")) {
+            Toast.makeText(this, "Password is empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(!email.contains("@")) {
+            Toast.makeText(this, "Invalid Email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+    
+    
 }
--- app/src/main/java/com/example/ia_computerscience/Controller/SignUpActivity.java
+++ app/src/main/java/com/example/ia_computerscience/Controller/SignUpActivity.java
@@ -2,9 +2,16 @@ package com.example.ia_computerscience.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.ia_computerscience.Model.User;
 import com.example.ia_computerscience.R;
+import com.example.ia_computerscience.Util.Constants;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -16,9 +23,95 @@ public class SignUpActivity extends AppCompatActivity {
     private FirebaseFirestore firestore;
     private FirebaseUser mUser;
 
+    private EditText txtName;
+    private EditText txtEmail;
+    private EditText txtPassword;
+    private EditText txtPassword2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+        txtName = findViewById(R.id.SignUp_txtName);
+        txtEmail = findViewById(R.id.SignUp_txtEmail);
+        txtPassword = findViewById(R.id.SignUp_txtPassword);
+        txtPassword2 = findViewById(R.id.SignUp_txtPassword2);
+    }
+
+    public void signUp(View view) {
+        if(!formValid()) {
+            return;
+        }
+
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        //creating user with firebase authentication
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if(task.isSuccessful()) {
+                        mUser = mAuth.getCurrentUser();
+                        addUser(); //adding the newly created user to database
+                        startActivity(new Intent(this, MyRecipeActivity.class));
+                    }
+                    else {
+                        Toast.makeText(this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        Log.w(TAG, task.getException().getMessage(), task.getException());
+                    }
+                });
+
+    }
+
+    private boolean formValid() {
+        String name = txtName.getText().toString();
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+        String password2 = txtPassword2.getText().toString();
+
+        if(name.equals("")) {
+            Toast.makeText(this, "Name is empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(email.equals("")) {
+            Toast.makeText(this, "Email is empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(password.equals("") || password2.equals("")) {
+            Toast.makeText(this, "Password is empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(!email.contains("@")) {
+            Toast.makeText(this, "Invalid Email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(!password.equals(password2)) {
+            Toast.makeText(this, "Password confirmation failed", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void addUser() {
+        String name = txtName.getText().toString();
+        String uid = mUser.getUid();
+        String email = mUser.getEmail();
+
+        User user = new User(name, uid, email);
+
+        try {
+            firestore.collection(Constants.USER).document(uid).set(user);
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Error adding user to database", Toast.LENGTH_LONG).show();
+            Log.w(TAG, "Error adding user to database", e);
+        }
     }
 }
